5f7389ce2f4432704fca649802132d6aceea6c59
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-03-01 13:43:51 +0100

    Tests config and files

---
+++ server/src/test/java/nl/tudelft/oopp/livechat/MainApplicationTest.java
@@ -0,0 +1,14 @@
+package nl.tudelft.oopp.livechat;
+
+import nl.tudelft.oopp.livechat.config.H2Config;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {MainApplication.class, H2Config.class})
+public class MainApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ server/src/test/java/nl/tudelft/oopp/livechat/controllers/LectureControllerTest.java
@@ -0,0 +1,42 @@
+package nl.tudelft.oopp.livechat.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.tudelft.oopp.livechat.entities.LectureEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LectureControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getLecturesByID() throws Exception {
+        MvcResult result = this.mockMvc.perform(post("/post")).andExpect(status().isOk())
+                .andReturn();
+        String l = result.getResponse().getContentAsString();
+        LectureEntity lectureEntity = new ObjectMapper().readValue(l, LectureEntity.class);
+        String uuid = lectureEntity.getUuid();
+        String m = this.mockMvc.perform(get("/get/"+uuid)).andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals(l, m);
+    }
+
+    @Test
+    void whenValidInput_thenReturns200() throws Exception {
+        this.mockMvc.perform(post("/post")).andExpect(status().isOk());
+        this.mockMvc.perform(get("/get")).andExpect(status().is4xxClientError());
+    }
+}
