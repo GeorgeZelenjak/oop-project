2019e29c6f293e528c804dd6b42f6907a6f1e440
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-03-01 15:11:13 +0100

    solved style problems in tests

--- server/src/test/java/nl/tudelft/oopp/livechat/MainApplicationTest.java
+++ server/src/test/java/nl/tudelft/oopp/livechat/MainApplicationTest.java
@@ -4,8 +4,6 @@ import nl.tudelft.oopp.livechat.config.H2Config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest(classes = {MainApplication.class, H2Config.class})
 public class MainApplicationTest {
 
