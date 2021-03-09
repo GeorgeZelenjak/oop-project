15147e13f2fc46d217113c0b4268105f7b21639b
Artjom Pugatsov <a.pugatsov@student.tudelft.nl>
2021-03-09 12:41:42 +0200

    Changed formatting of ServerCommunication class.
    Added comments and indentation

--- client/src/main/java/nl/tudelft/oopp/livechat/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/livechat/communication/ServerCommunication.java
@@ -8,8 +8,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.UUID;
 
 import nl.tudelft.oopp.livechat.data.Lecture;
@@ -20,51 +18,75 @@ import nl.tudelft.oopp.livechat.data.QuestionEntity;
  */
 public class ServerCommunication {
 
-    private static HttpClient client = HttpClient.newBuilder().build();
-    static Gson gson = new GsonBuilder().setDateFormat(
+    //Client object for sending requests
+    private static final HttpClient client = HttpClient.newBuilder().build();
+    /* Gson object for parsing Json
+    set to parse fields according to annotations
+    and with specified date format
+     */
+    private static final Gson gson = new GsonBuilder().setDateFormat(
             "EEE, dd MMM yyyy HH:mm:ss zzz").excludeFieldsWithoutExposeAnnotation().create();
 
 
     /**
      * Creates a new lecture.
      * @param name      A name of the lecture
-     * @return          Lecture which was created, null otherwise
+     * @return          Lecture which was created, null in case of errors
     */
     //  TODO: I AM PASSING A BLANK STRING IN THE POST METHOD, THIS SHOULD BE CHANGED
     public static Lecture createLecture(String name) {
+
+        //Encoding the lecture name into url compatible format
         name = URLEncoder.encode(name, StandardCharsets.UTF_8);
-        HttpRequest request = HttpRequest.newBuilder().POST(HttpRequest.BodyPublishers
-                .ofString("")).uri(URI.create("http://localhost:8080//api/newLecture?name=" + name)).build();
+
+        //Parameters for request
+        HttpRequest.BodyPublisher req = HttpRequest.BodyPublishers.ofString("");
+        String address = "http://localhost:8080//api/newLecture?name=";
+
+        //Creating request and defining response
+        HttpRequest request = HttpRequest.newBuilder().POST(req)
+                .uri(URI.create(address + name)).build();
         HttpResponse<String> response;
+
+        //Catching error when communicating with server
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+
+        // Prints the status code if the communication
+        // if server gives an unexpected response
         if (response.statusCode() != 200) {
             System.out.println("Status: " + response.statusCode());
         }
 
+        //Return object from response
         return gson.fromJson(response.body(), Lecture.class);
 
     }
 
     /**
-     * Sends an HTTP request to get lecture
-     * by uuid.
+     * Sends an HTTP request to get lecture by uuid.
      *
      * @param lectureId The uuid of the lecture
      * @return Lecture if the lecture exists on server or null if it doesn't
      */
     public static Lecture joinLectureById(String lectureId) {
+
+        //Encoding the lecture id into url compatible format
         lectureId = URLEncoder.encode(lectureId, StandardCharsets.UTF_8);
 
+        //Parameter for request
+        String address = "http://localhost:8080/api/get/";
 
+        //Creating request and defining response
         HttpRequest request = HttpRequest.newBuilder().GET().uri(
-                URI.create("http://localhost:8080/api/get/" + lectureId)).build();
+                URI.create(address + lectureId)).build();
         HttpResponse<String> response;
 
+        //Catching error when communicating with server
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e) {
@@ -75,6 +97,8 @@ public class ServerCommunication {
             System.out.println("Status: " + response.statusCode());
             return null;
         }
+
+        //Return object from response
         return gson.fromJson(response.body(), Lecture.class);
     }
 
@@ -83,28 +107,38 @@ public class ServerCommunication {
      * a question with the current LectureID.
      *
      * @param questionText the question text
-     * @return the int
+     * @return integer representing servers response
+     *      or the lack of it
+     *       1 - Question has been successfully asked
+     *      -1 - Current lecture not set
+     *      -2 - No response from server
+     *      -3 - Unexpected response from server
      */
     public static int askQuestion(String questionText) {
 
+        //Checking if current lecture has been set
         if (Lecture.getCurrentLecture() == null) {
             System.out.println("You are not connected to a lecture!");
             return -1;
         }
 
-
+        //Parameters for question
         UUID lectureId = Lecture.getCurrentLecture().getUuid();
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         QuestionEntity question = new QuestionEntity(lectureId,questionText,  42);
 
+        //Parameters for request
         String json = gson.toJson(question);
         HttpRequest.BodyPublisher req =  HttpRequest.BodyPublishers.ofString(json);
+        String address = "http://localhost:8080/api/question/ask";
+        String headerName = "Content-Type";
+        String headerValue = "application/json";
 
+        //Creating request and defining response
         HttpRequest request = HttpRequest.newBuilder().POST(req).uri(
-                URI.create("http://localhost:8080/api/question/ask")).setHeader(
-                        "Content-Type", "application/json").build();
+                URI.create(address)).setHeader(headerName, headerValue).build();
         HttpResponse<String> response;
 
+        //Catching error when communicating with server
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e) {
@@ -112,12 +146,15 @@ public class ServerCommunication {
             e.printStackTrace();
             return -2;
         }
+
+        //Unexpected response
         if (response.statusCode() != 200) {
             System.out.println("Status: " + response.statusCode());
             return -3;
         }
-        System.out.println("The question was asked successfully! " + response.body());
 
+        //Question has been asked successfully
+        System.out.println("The question was asked successfully! " + response.body());
         return 1;
     }
 
