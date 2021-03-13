c2892719d0ae8f53d6923e48a0a49d0da63082d3
tpopica <T.G.Popica@student.tudelft.nl>
2021-03-14 01:10:02 +0200

    Testing for LectureCommunication with Artjom.

--- client/src/test/java/nl/tudelft/oopp/livechat/communication/LectureCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/livechat/communication/LectureCommunicationTest.java
@@ -4,20 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import nl.tudelft.oopp.livechat.data.Lecture;
 import nl.tudelft.oopp.livechat.servercommunication.LectureCommunication;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
+import org.mockserver.matchers.Times;
 import org.mockserver.model.HttpResponse;
+import org.mockserver.verify.VerificationTimes;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockserver.model.HttpRequest.request;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LectureCommunicationTest {
 
     public static MockServerClient mockServer;
-
+    public static boolean stopServer = false;
 
     /**
      * Starts mock server.
@@ -39,10 +40,9 @@ public class LectureCommunicationTest {
         String jsonResponseLecture = node.toString();
         final String  responseQuestionBody = "5397545054934456486";
 
-
         mockServer = ClientAndServer.startClientAndServer(8080);
         mockServer.when(request().withMethod("POST").withPath("/api/newLecture")
-                .withQueryStringParameter("name","name"))
+                .withQueryStringParameter("name","name"), Times.exactly(2))
                 .respond(HttpResponse.response().withStatusCode(200)
                         .withBody(jsonResponseLecture)
                         .withHeader("Content-Type","application/json"));
@@ -52,7 +52,6 @@ public class LectureCommunicationTest {
                 .respond(HttpResponse.response().withStatusCode(200)
                         .withBody(jsonResponseLecture)
                         .withHeader("Content-Type","application/json"));
-
 
         mockServer.when(request().withMethod("POST").withPath("/api/question/ask"))
                 .respond(HttpResponse.response().withStatusCode(200)
@@ -68,14 +67,18 @@ public class LectureCommunicationTest {
                 .withPath("/api/close/12/69"))
                 .respond(HttpResponse.response().withStatusCode(200)
                         .withBody("0"));
+
+        mockServer.verify(request(), VerificationTimes.exactly(0));
     }
 
     @Test
+    @Order(1)
     public void testCreateLectureNotNull() {
         assertNotNull(LectureCommunication.createLecture("name"));
     }
 
     @Test
+    @Order(2)
     public void testLectureNameMatches() {
         Lecture res = LectureCommunication.createLecture("name");
         assertNotNull(res);
@@ -83,38 +86,49 @@ public class LectureCommunicationTest {
     }
 
     @Test
+    @Order(3)
     public void joinLectureByIdLectureExists() {
         Lecture res = LectureCommunication.joinLectureById("0ee81155-96fc-4045-bfe9-dd7ca714b5e8");
         assertNotNull(res);
         assertEquals("0ee81155-96fc-4045-bfe9-dd7ca714b5e8", res.getUuid().toString());
-
     }
 
     @Test
+    @Order(4)
     public void joinLectureByIdNotExist() {
         Lecture res = LectureCommunication.joinLectureById("zebra");
         assertNull(res);
     }
 
     @Test
+    @Order(5)
     public void validateModeratorPass() {
         assertTrue(LectureCommunication.validateModerator("112","123"));
     }
 
     @Test
+    @Order(6)
     public void validateModeratorFail() {
         assertFalse(LectureCommunication.validateModerator("not zebra","Zebra"));
     }
 
     @Test
+    @Order(7)
     public void closeLecturePass() {
         Lecture.setCurrentLecture(new Lecture());
         assertTrue(LectureCommunication.closeLecture("12","69"));
     }
 
     @Test
+    @Order(8)
     public void closeLectureFail() {
         assertFalse(LectureCommunication.closeLecture("112","123"));
+    }
+
+    @Test
+    @Order(9)
+    public void testCreateLectureNoRespose() {
+        assertNull(LectureCommunication.createLecture("name"));
     }
 
     /**
