f0a1099d04e20e0f6a583914be2a50699c523bf3
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-03-13 11:59:20 +0100

    fixed generation of time when using normal constructor so tests don't fail

--- server/src/test/java/nl/tudelft/oopp/livechat/entities/QuestionEntityTest.java
+++ server/src/test/java/nl/tudelft/oopp/livechat/entities/QuestionEntityTest.java
@@ -13,14 +13,14 @@ class QuestionEntityTest {
     private static final UUID lectureId = UUID.randomUUID();
     private static final Long ownerId = ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
     private static QuestionEntity questionEntity;
-    private static final Timestamp time = new Timestamp(System.currentTimeMillis());
+    private static final Timestamp time = new Timestamp(System.currentTimeMillis() / 1000 * 1000);
 
     @BeforeAll
     static void setUp() {
         questionEntity = new QuestionEntity(lectureId,
                 "What is the answer to the Ultimate "
                         + "Question of Life, the Universe, and Everything?",
-                new Timestamp(System.currentTimeMillis()), ownerId);
+                null, ownerId);
     }
 
     @Test
