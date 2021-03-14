380d283323dd3f0d673b8d524f241fbaf1cfbf0a
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2021-03-14 21:27:38 +0100

    Add modDeleteServerRefusesTest in QuestionCommunicationTest

--- client/src/test/java/nl/tudelft/oopp/livechat/communication/QuestionCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/livechat/communication/QuestionCommunicationTest.java
@@ -210,7 +210,7 @@ public class QuestionCommunicationTest {
     private static void createExpectationsForModDelete() {
         //Success
         mockServer.when(request().withMethod("DELETE").withPath("/api/question/moderator/delete")
-                .withQueryStringParameters(new Parameter("qid", qid1),
+                .withQueryStringParameters(new Parameter("qid", qid2),
                         new Parameter("modkey", String.valueOf(modkey))))
                 .respond(HttpResponse.response().withStatusCode(200)
                         .withBody("0").withHeader("Content-Type","application/json"));
@@ -231,7 +231,7 @@ public class QuestionCommunicationTest {
 
         //qid not found
         mockServer.when(request().withMethod("DELETE").withPath("/api/question/moderator/delete")
-                .withQueryStringParameters(new Parameter("qid", qid2),
+                .withQueryStringParameters(new Parameter("qid", "666"),
                         new Parameter("modkey", String.valueOf(modkey))))
                 .respond(HttpResponse.response().withStatusCode(200)
                         .withBody("-1").withHeader("Content-Type","application/json"));
@@ -538,7 +538,7 @@ public class QuestionCommunicationTest {
                 modkey, "Assembly", "Otto"));
         QuestionCommunication.askQuestion("Is there anybody?");
 
-        assertEquals(0, QuestionCommunication.modDelete(Long.parseLong(qid1), modkey));
+        assertEquals(0, QuestionCommunication.modDelete(Long.parseLong(qid2), modkey));
     }
 
     @Test
@@ -546,6 +546,16 @@ public class QuestionCommunicationTest {
         Lecture.setCurrentLecture(null);
 
         assertEquals(-1, QuestionCommunication.deleteQuestion(Long.parseLong(qid1), 443));
+    }
+
+    @Test
+    public void modDeleteServerRefusesTest() {
+        stopServer();
+        Lecture.setCurrentLecture(new Lecture(lid, modkey,
+                "CPU", "Koen"));
+
+        assertEquals(-2, QuestionCommunication.deleteQuestion(42, 42));
+        startServer();
     }
 
     /**
