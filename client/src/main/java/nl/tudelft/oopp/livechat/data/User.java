3628183b688c0b9ff1086f8c03493629fdcd73d8
tpopica <T.G.Popica@student.tudelft.nl>
2021-03-14 12:16:24 +0200

    JavaDoc for data client side.

--- client/src/main/java/nl/tudelft/oopp/livechat/data/User.java
+++ client/src/main/java/nl/tudelft/oopp/livechat/data/User.java
@@ -1,23 +1,43 @@
 package nl.tudelft.oopp.livechat.data;
 
-
+/**
+ * User class.
+ */
 public class User {
     private static String userName;
 
     private static long uid;
 
+    /**
+     * Sets uid.
+     */
     public static void setUid() {
         uid = (long) (Math.random() * 10000);
     }
 
+    /**
+     * Gets uid.
+     *
+     * @return the uid
+     */
     public static long getUid() {
         return uid;
     }
 
+    /**
+     * Sets user name.
+     *
+     * @param name the name
+     */
     public static void setUserName(String name) {
         userName = name;
     }
 
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
     public static String getUserName() {
         return userName;
     }
