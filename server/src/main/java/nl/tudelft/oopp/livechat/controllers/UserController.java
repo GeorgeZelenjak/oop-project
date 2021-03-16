17f1fb3fb9e95e77b49d8bea7907ca5848b74eb9
Giulio Segalini <g.segalini@student.tudelft.nl>
2021-03-16 17:41:10 +0100

    everything to work with users in database and API

---
+++ server/src/main/java/nl/tudelft/oopp/livechat/controllers/UserController.java
@@ -0,0 +1,49 @@
+package nl.tudelft.oopp.livechat.controllers;
+
+import nl.tudelft.oopp.livechat.entities.UserEntity;
+import nl.tudelft.oopp.livechat.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * POST endpoint to create a new user in the database.
+     *
+     * @param user the user
+     * @return the int
+     */
+    @PostMapping("/register")
+    public int newUser(@RequestBody UserEntity user) {
+        String remoteAddress = ((ServletRequestAttributes)
+                RequestContextHolder.currentRequestAttributes())
+                .getRequest().getRemoteAddr();
+        return userService.newUser(user, remoteAddress);
+    }
+
+    /**
+     * Exception handler.
+     * @param exception exception that has occurred
+     * @return response body with 400 and 'Invalid UUID' message
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> badUUID(IllegalArgumentException exception) {
+        System.out.println(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Invalid UUID");
+    }
+
+}
