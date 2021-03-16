3773c92305541396391a44a0ce4f855e549a60ec
Jegor Zelenjak <J.Zelenjak@student.tudelft.nl>
2021-03-16 21:22:14 +0100

    Add javadoc and tests for UserQuestionPairId.

--- server/src/main/java/nl/tudelft/oopp/livechat/entities/UserQuestionPairId.java
+++ server/src/main/java/nl/tudelft/oopp/livechat/entities/UserQuestionPairId.java
@@ -3,45 +3,86 @@ package nl.tudelft.oopp.livechat.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Class to store a combination of user id and question id
+ *       to track which questions the user has asked.
+ */
 public class UserQuestionPairId implements Serializable {
 
+    private long userId;
 
-    long userId;
+    private long questionId;
 
-    long questionId;
-
+    /**
+     * Creates a new UserQuestionPairId object.
+     */
     public UserQuestionPairId() {
     }
 
+    /**
+     * Creates a new UserQuestionPairId object with the specified parameters.
+     * @param userId the id of the user
+     * @param questionId the id of the question
+     */
     public UserQuestionPairId(long userId, long questionId) {
         this.userId = userId;
         this.questionId = questionId;
     }
 
+    /**
+     * Gets the id of the user.
+     * @return the id of the user
+     */
     public long getUserId() {
-        return userId;
+        return this.userId;
     }
 
+    /**
+     * Sets the id of the user.
+     * @param userId the id of the user
+     */
     public void setUserId(long userId) {
         this.userId = userId;
     }
 
+    /**
+     * Gets the id of the question.
+     * @return the id of the question
+     */
     public long getQuestionId() {
-        return questionId;
+        return this.questionId;
     }
 
+    /**
+     * Sets the id of the question.
+     * @param questionId the id of the question
+     */
     public void setQuestionId(long questionId) {
         this.questionId = questionId;
     }
 
+    /**
+     * Compares the UserQuestionPairId to another object.
+     * @param o the object to compare to
+     * @return true iff another object is also UserQuestionPairId
+     *          and has the same user id and question id.
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserQuestionPairId that = (UserQuestionPairId) o;
-        return userId == that.userId && questionId == that.questionId;
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof UserQuestionPairId) {
+            UserQuestionPairId that = (UserQuestionPairId) o;
+            return userId == that.userId && questionId == that.questionId;
+        }
+        return false;
     }
 
+    /**
+     * Creates the hash code for the object.
+     * @return the created hash code
+     */
     @Override
     public int hashCode() {
         return Objects.hash(userId, questionId);
