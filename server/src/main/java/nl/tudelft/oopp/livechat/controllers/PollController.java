1c9c7a7c368b0e5dc4588b45375b0ca8de898f08
PreliminaryOD <o.danilov@student.tudelft.nl>
2021-03-23 18:15:51 +0100

    Poll controller

---
+++ server/src/main/java/nl/tudelft/oopp/livechat/controllers/PollController.java
@@ -0,0 +1,57 @@
+package nl.tudelft.oopp.livechat.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import nl.tudelft.oopp.livechat.entities.PollAndOptions;
+import nl.tudelft.oopp.livechat.entities.PollEntity;
+import nl.tudelft.oopp.livechat.entities.PollOptionEntity;
+import nl.tudelft.oopp.livechat.services.PollService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/poll")
+public class PollController {
+    private final PollService pollService;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public PollController(PollService pollService) {
+        this.pollService = pollService;
+    }
+
+    @PutMapping("/create/{uuid}/{modkey}")
+    public PollEntity createPoll(@PathVariable UUID uuid, @PathVariable UUID modkey,
+                                 @RequestBody String questionText) {
+        return pollService.createPoll(uuid, modkey, questionText);
+    }
+
+    @PutMapping("/addOption/{pollId}/{modkey}/{isCorrect}")
+    public PollOptionEntity addOption(@PathVariable long pollId, @PathVariable UUID modkey,
+                                      @PathVariable boolean isCorrect,
+                                      @RequestBody String optionText) {
+        return pollService.addOption(pollId, modkey, optionText, isCorrect);
+    }
+
+    @PutMapping("/toggle/{pollId}/{modkey}")
+    public int addOption(@PathVariable long pollId, @PathVariable UUID modkey) {
+        return pollService.togglePoll(pollId, modkey);
+    }
+
+    @GetMapping("/fetch/{uuid}")
+    public PollAndOptions fetchPollAndOptions(@PathVariable UUID uuid) {
+        return pollService.fetchPollAndOptions(uuid);
+    }
+
+    @PutMapping("/vote/{userId}/{pollOptionId}")
+    public int voteOnPoll(@PathVariable long userId, @PathVariable long pollOptionId) {
+        return pollService.voteOnPoll(userId,pollOptionId);
+    }
+
+    @DeleteMapping("/reset/{pollId}/{modkey}")
+    public int resetVotes(@PathVariable long pollId, @PathVariable UUID modkey) {
+        return pollService.resetVotes(pollId,modkey);
+    }
+
+
+}
